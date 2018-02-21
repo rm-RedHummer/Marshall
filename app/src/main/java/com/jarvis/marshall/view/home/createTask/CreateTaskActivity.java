6fda77e3383ee5707de4cdb13c3c4f0a29b39ef3
rm-RedHummer <goodmage019@gmail.com>
2018-02-21 08:20:23 +0800

    Create task xml

--- app/src/main/java/com/jarvis/marshall/view/home/createTask/CreateTaskActivity.java
+++ app/src/main/java/com/jarvis/marshall/view/home/createTask/CreateTaskActivity.java
@@ -1,15 +1,32 @@
 package com.jarvis.marshall.view.home.createTask;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.jarvis.marshall.R;
+import com.jarvis.marshall.view.home.createEvent.CreateEventFragment;
 
 public class CreateTaskActivity extends AppCompatActivity {
-
+    private String eventKey;
+    private FragmentManager fm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+        eventKey = getIntent().getExtras().getString("eventKey");
+        fm = getSupportFragmentManager();
+        changeFragment(new CreateTaskFragment(),"CreateTaskFragment");
+    }
+
+    public void changeFragment(Fragment fragment, String tag){
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.actCreateTask_FrameLayout, fragment, tag);
+        //ft.replace(R.id.main_framelayout,fragment,tag);
+        ft.setCustomAnimations(R.anim.enter_anim,R.anim.stay_anim,R.anim.stay_anim,R.anim.exit_anim);
+        ft.addToBackStack(tag);
+        ft.commit();
     }
 }
--- app/src/main/java/com/jarvis/marshall/view/home/createTask/CreateTaskFragment.java
+++ app/src/main/java/com/jarvis/marshall/view/home/createTask/CreateTaskFragment.java
@@ -6,14 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.jarvis.marshall.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CreateTaskFragment extends Fragment {
-
+public class CreateTaskFragment extends Fragment implements View.OnClickListener{
+    private View view;
+    private Button detailsBtn, dateBtn, TimeBtn, membersBtn, cancelBtn,saveBtn;
 
     public CreateTaskFragment() {
         // Required empty public constructor
@@ -24,7 +26,33 @@ public class CreateTaskFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_task, container, false);
+        view = inflater.inflate(R.layout.fragment_create_task, container, false);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case(R.id.actCreateTask_detailsBtn):
+
+                break;
+            case(R.id.actCreateTask_deadlineDateBtn):
+
+                break;
+
+            case(R.id.actCreateTask_deadlineTimeBtn):
+
+                break;
+            case(R.id.actCreateTask_members):
+
+                break;
+            case(R.id.actCreateTask_cancelBtn):
+
+                break;
+            case(R.id.actCreateTask_saveBtn):
+
+                break;
+        }
+    }
 }
